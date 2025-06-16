abe171a2801142fe135dbdeb31e93f46afc6b2ba
Kelvin <machakakelvin903@gmail.com>
2025-06-16 10:53:53 +0200

    Wire up React build in Django; configure static/templates

--- meterdatalogger/urls.py
+++ meterdatalogger/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.authtoken import views as drf_views
 from django.views.generic import TemplateView
-from django.conf.urls.static import static
 from django.conf import settings
-from django.urls import path, include, re_path
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,10 +11,10 @@ urlpatterns = [
     path('api-token-auth/', drf_views.obtain_auth_token),
 ]
 
-# Catch-all: serve React's index.html for all other paths
+# Catch-all to serve React's index.html for all other URLs
 urlpatterns += [
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
 
-# In case you're serving static via Django (WhiteNoise handles this automatically)
+# Serve static files (collected by collectstatic)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
